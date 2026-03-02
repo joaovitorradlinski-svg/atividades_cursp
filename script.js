2a93d3caff9061b59119199635428db0a74a3e12
joão vitor radlinski <joaovitor.radlinski@gmail.com>
2026-03-02 14:43:53 -0300

    barra de progresso não funcionando

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const currentprogress = document.getElementById('current-progress');
 const progresscontainer = document.getElementById('progress-container');
 const shuffle = document.getElementById('shuffle');
 const repeat = document.getElementById('repeat');
+const currentTime = document.getElementById('current-time');
+const duration = document.getElementById('duration');
 
 
 
@@ -32,7 +34,10 @@ const extra ={
 }
 
 let isPlaying = false;
+let issnuffle =false;
+let isrepeat = false;
 const playlist =[extra, iwillsurvive, lovethesubhumanself]
+let sortedplaylist = [...playlist]
 let index = 0;
 
 
@@ -51,15 +56,15 @@ function pauseSong(){
 }
 
 function loadSong(){
-    cover.src = `images/${playlist[index].file}.jpeg`;
-    song.src = `songs/${playlist[index].file}.mp3`
-    SongName.innerText = playlist[index].SongName;
-    bandName.innerText = playlist[index].bandName;
+    cover.src = `images/${sortedplaylist[index].file}.jpeg`;
+    song.src = `songs/${sortedplaylist[index].file}.mp3`
+    SongName.innerText = sortedplaylist[index].SongName;
+    bandName.innerText = sortedplaylist[index].bandName;
 }
 
 function previoussong(){
     if (index === 0){
-        index = playlist.length -1;
+        index = sortedplaylist.length -1;
     }
     else{
         index -=1;
@@ -69,7 +74,7 @@ function previoussong(){
 }
 
 function nextsong(){
-    if (index === playlist.length -1){
+    if (index === sortedplaylist.length -1){
         index = 0;
     }
     else{
@@ -89,15 +94,70 @@ function updateprogressbar(){
 
 
 function jumpto(event){
-    const width =progresscontainer.clientWidth
+    const width =progresscontainer.clientWidth;
     const clickposition = event.offsetX;
-    song.jumpToTime = (clickposition / width) * song.duration
-    song.currentTime = jumpToTime
+    song.jumpToTime = (clickposition / width) * song.duration;
+    song.currentTime = jumpToTime;
+    song.currenttime=jumpToTime;
+    alert(song.currentTime);
 
 
 }
 
+function snufflearray(presnuffledarray){
+    let size = sortedplaylist.length;
+    let currentIndex = size -1;
+    while(currentIndex > 0){
+        let randomIndex = Math.floor(Math.random()* size);
+        let aux = presnuffledarray[currentIndex];
+        presnuffledarray[currentIndex] = presnuffledarray[randomIndex];
+        presnuffledarray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
 
+
+function shufflebuttoncliked(){
+    if(issnuffle === false){
+        issnuffle = true
+        snufflearray(sortedplaylist)
+        shuffle.classList.add('button-shuffle')
+    }
+    else{
+        issnuffle = false
+        shuffle.classList.remove('button-shuffle')
+    }
+
+
+}
+
+function repeatbuttoncliked(){
+    if(isrepeat === false){
+        isrepeat = true
+        repeat.classList.add('button-repeat')
+    }
+    else{
+        isrepeat = false
+        repeat.classList.remove('button-repeat')
+    }
+}
+function nextorRepeat(){
+    if(isrepeat === false   ){
+        nextsong();
+    }
+    else{
+        pauseSong();
+    }
+}
+function formatTime(time){
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds}`;
+}
+
+function updatetotaltime(){
+    duration.innerText = formatTime(song.duration);
+}
 
 loadSong();
 
@@ -110,7 +170,14 @@ play.addEventListener('click', () => {
         playSong();
     }
 })
+
+
 prev.addEventListener('click', previoussong);
 next.addEventListener('click', nextsong);
 song.addEventListener('timeupdate', updateprogressbar);
 progresscontainer.addEventListener('click', jumpto);
+song.addEventListener('ended', nextorRepeat);
+song.addEventListener('loadedmetadata', updatetotaltime);
+shuffle.addEventListener('click' ,shufflebuttoncliked
+)
+repeat.addEventListener('click', repeatbuttoncliked)
